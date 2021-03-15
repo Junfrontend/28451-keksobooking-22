e653e13b99ce2c5f6bea12e177e626181888d37d
Данил <danil.beloborodoff@yandex.ru>
2021-03-15 13:11:02 +0300

    Исправил ошибки и структуру данных

--- js/main.js
+++ js/main.js
@@ -47,9 +47,7 @@ function randomNumberFromToWithFloat(min, max, digitAfterComma) {
   }
 }
 
-let author = {
-  avatar: 'img/avatars/user0' + randomIntNumberFromTo(1, 8) + 'png',
-}
+
 
 let typeOfAccommodaion = ['palace', 'flat', 'house', 'bungalow'];
 let timeOfCheckinOut = ['12:00', '13:00', '14:00'];
@@ -71,30 +69,36 @@ let getSomePhotos = function () {
   return photosList;
 }
 
-let offer = {
-  title: 'Самые мурчательные варианты размещения - Кексобукинг',
-  address: 'X',
-  price: randomIntNumberFromTo(1000, 8000),
-  type: typeOfAccommodaion[randomIntNumberFromTo(0, typeOfAccommodaion.length - 1)],
-  rooms: randomIntNumberFromTo(1, 14),
-  guests: randomIntNumberFromTo(1, 14),
-  checkin: timeOfCheckinOut[randomIntNumberFromTo(0, typeOfAccommodaion.length - 1)],
-  checkout: timeOfCheckinOut[randomIntNumberFromTo(0, typeOfAccommodaion.length - 1)],
-  features: getRandomArrayOfFeatures(),
-  description: 'Обмурчательный вариант! Есть миска и лоток!',
-  photos: getSomePhotos(),
-}
-let locationData = {
-  x: randomNumberFromToWithFloat(35.65000, 35.70000, 5),
-  y: randomNumberFromToWithFloat(139.70000, 139.80000, 5),
-}
 
 let getTestData = function () {
   let DataTestArray = [];
   for (let i = 0; DataTestArray.length < 10; i++) {
-    getTestData[i] = [author, offer, locationData];
+    getTestData[i] = {
+      author: {
+        avatar: 'img/avatars/user0' + randomIntNumberFromTo(1, 8) + 'png',
+      },
+      offer: {
+        title: 'Самые мурчательные варианты размещения - Кексобукинг',
+        address: 'X',
+        price: randomIntNumberFromTo(1000, 8000),
+        type: typeOfAccommodaion[randomIntNumberFromTo(0, typeOfAccommodaion.length - 1)],
+        rooms: randomIntNumberFromTo(1, 14),
+        guests: randomIntNumberFromTo(1, 14),
+        checkin: timeOfCheckinOut[randomIntNumberFromTo(0, typeOfAccommodaion.length - 1)],
+        checkout: timeOfCheckinOut[randomIntNumberFromTo(0, typeOfAccommodaion.length - 1)],
+        features: getRandomArrayOfFeatures(),
+        description: 'Обмурчательный вариант! Есть миска и лоток!',
+        photos: getSomePhotos(),
+      },
+      location: {
+        x: randomNumberFromToWithFloat(35.65000, 35.70000, 5),
+        y: randomNumberFromToWithFloat(139.70000, 139.80000, 5),
+      },
+    };
     DataTestArray.push(getTestData[i]);
   }
+  
   return DataTestArray;
+
 }
 getTestData();
